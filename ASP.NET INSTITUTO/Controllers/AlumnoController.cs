6bd7c5d14020b5c2b570fcb5018e4d21b55d5c88
Juan Ignacio skreka <skrekanacho@gmail.com>
2025-01-10 14:02:59 -0300

    primera version del proyecto instituto con asp.net

---
+++ ASP.NET INSTITUTO/Controllers/AlumnoController.cs
@@ -0,0 +1,17 @@
+﻿using ASP.NET_INSTITUTO.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_INSTITUTO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlumnoController : ControllerBase
+    {
+        [HttpGet(Name = "GetAlumnos")] //avisamos al controller que el método es un get, que traemos cosas
+        public List<Alumnos> GetAlumnos()
+        {
+            return new List<Alumnos>();
+        }
+        
+    }
+}
---
+++ ASP.NET INSTITUTO/Model/Alumnos.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP.NET_INSTITUTO.Model
+{
+    public class Alumnos
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int dni { get; set; }
+        public DateTime nacimiento { get; set; }
+
+
+    }
+}
